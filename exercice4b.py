ee6b0cfb780197397dd0e398a9da48b470f9af11
zhongzheng-he <65607132+zhongzheng-he@users.noreply.github.com>
2022-03-16 13:11:29 +0100

    Update exercice4b.py

--- exercice4b.py
+++ exercice4b.py
@@ -4,16 +4,19 @@ TP TNI Python : Resolution VS Definition
 
 @author: emilien
 """
-import scipy.misc as misc 
+from skimage.transform import resize
 import matplotlib.pyplot as plt
 import iptools.imdisplaytools as imdisp
 import numpy as np
+import imageio as iio
 
-lenaIm = misc.imread("lena.png") #chargement de Lena
+lenaIm = iio.imread("lena.png") #chargement de Lena
 [rows,cols] = lenaIm.shape # retourne la taille (512x512)
-lenaIm2 = misc.imresize(lenaIm, [rows//2, cols//2]) # retaille copie de lenaIm de taille /2
-lenaIm3 = misc.imresize(lenaIm, [rows//4, cols//4]) # retaille copie de lenaIm de taille /4
-lenaIm4 = misc.imresize(lenaIm, [rows//8, cols//8]) # retaille copie de lenaIm de taille /8
+
+
+lenaIm2 = resize(lenaIm, [rows//2, cols//2]) # retaille copie de lenaIm de taille /2
+lenaIm3 = resize(lenaIm, [rows//4, cols//4]) # retaille copie de lenaIm de taille /4
+lenaIm4 = resize(lenaIm, [rows//8, cols//8]) # retaille copie de lenaIm de taille /8
 lenaIm5 = lenaIm.copy() # copie simplement lenaIm dans lenaIm5
 
 plt.figure()
